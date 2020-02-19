5418e17e3e8479d438dc8fe152a3a090c4050a58
michaelseg11 <michaelseg11@gmail.com>
2020-02-19 15:32:26 +0200

    try our facebook login btn

--- facebook_login/client/FacebookLogin.js
+++ facebook_login/client/FacebookLogin.js
@@ -1,42 +1,64 @@
-import React, { Component } from 'react';
-import FacebookLogin from 'react-facebook-login';
-import GenericTools from '../../../tools/GenericTools'; //change the location
-import Auth from '../../../auth/Auth';
+import React from 'react';
 import propTypes from 'prop-types';
 import './FacebookLogin.scss';
 
 function LoginWithFacebook(props) {
 
-
     const handleClick = e => {
         sessionStorage.setItem("login", "facebook");
+
+        const state = JSON.stringify({
+            successUrl: encodeURIComponent(window.location.origin + (window.location.hash[0] === "#" ? `/#${props.successUrl || '/samples'}` : `${props.successUrl || '/samples'}`)),
+            failUrl: encodeURIComponent(window.location.origin + (window.location.hash[0] === "#" ? `/#${props.failureUrl || '/'}` : `${props.failureUrl || '/'}`))
+        });
+
+        const url = `https://www.facebook.com/v6.0/dialog/oauth?client_id=${props.appId}&redirect_uri=${process.env.REACT_APP_SERVER_DOMAIN}/fbcallback/&state=${state}`;
+        window.location.href = url;
     }
 
-    const state = JSON.stringify({
-        successUrl: window.location.origin + (window.location.hash[0] === "#" ? `/#${props.successUrl || '/samples'}` : `${props.successUrl || '/samples'}`),
-        failUrl: window.location.origin + (window.location.hash[0] === "#" ? `/#${props.failureUrl || '/'}` : `${props.failureUrl || '/'}`)
-    })
-
-    return (<div>
-        <FacebookLogin
-            appId={props.appId}
-            fields="name,email,picture"
-            autoLoad={false}
-            onFailure={res => console.log(res)}
-            reAuthenticate={true}
-            callback={e => console.log(e)}
-            // disableMobileRedirect={true}
-            isMobile={true}
-            redirectUri={process.env.REACT_APP_SERVER_DOMAIN + "/fbcallback/"}
-            textButton={"חשבון פייסבוק"}
-            cssClass="my-facebook-button"
-            icon="fa-facebook"
-            state={state}
-            onClick={handleClick}
-        />
-
-    </div >);
+    return (
+        <button className="my-facebook-button" onClick={handleClick}>
+            <i className="fa fa-facebook" />
+            חשבון פייסבוק
+        </button>
+    );
 }
+
+
+// function LoginWithFacebook(props) {
+
+
+//     const handleClick = e => {
+//         sessionStorage.setItem("login", "facebook");
+//     }
+
+//     const state = JSON.stringify({
+//         successUrl: window.location.origin + (window.location.hash[0] === "#" ? `/#${props.successUrl || '/samples'}` : `${props.successUrl || '/samples'}`),
+//         failUrl: window.location.origin + (window.location.hash[0] === "#" ? `/#${props.failureUrl || '/'}` : `${props.failureUrl || '/'}`)
+//     })
+
+//     return (<div>
+// <FacebookLogin
+//             appId={props.appId}
+//             fields="name,email,picture"
+//             autoLoad={false}
+//             onFailure={res => console.log(res)}
+//             reAuthenticate={true}
+//             callback={e => console.log(e)}
+//             // disableMobileRedirect={true}
+//             isMobile={true}
+//             redirectUri={process.env.REACT_APP_SERVER_DOMAIN + "/fbcallback/"}
+//             textButton={"חשבון פייסבוק"}
+//             cssClass="my-facebook-button"
+//             icon="fa-facebook"
+//             state={state}
+//             onClick={handleClick}
+//         />
+
+//     </div >);
+// }
+
+
 LoginWithFacebook.propTypes = {
     appId: propTypes.string,
     successUrl: propTypes.string,
